9916d480079b8fbff11128e76e953ee663b09ff7
Moshik Baruch <moshik1526@gmail.com>
2021-02-16 21:23:54 +0200

    Create ThirdRun.ps1

---
+++ ThirdRun.ps1
@@ -0,0 +1,28 @@
+function Extend ($TimesRun)
+{
+    [int]$TimesRun = [int]$TimesRun + 1
+    [System.Environment]::SetEnvironmentVariable('TimesRun',"$TimesRun",'Machine')
+}
+
+Write-Host "My job name is: $ENV:JOB_NAME"
+$Third = $false
+try
+{
+	$TimesRun = [System.Environment]::GetEnvironmentVariable('TimesRun','Machine')
+}
+catch
+{
+    write-host $TimesRun
+    [System.Environment]::SetEnvironmentVariable('TimesRun','1','Machine')
+}
+$TimesRun = [System.Environment]::GetEnvironmentVariable('TimesRun','Machine')
+if([int]$TimesRun % 3 -eq 0)
+{
+    write-host "Third run!"
+    Extend($TimesRun)
+}
+else
+{
+    Extend($TimesRun)
+    Write-Error "Not the third run!"
+}
